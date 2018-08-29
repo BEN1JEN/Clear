0f63e44298d06f5ccc0d4acb3020ac6f72713aa8
BEN1JEN <benpheard@gmail.com>
2018-08-29 13:41:36 -0600

    Add voxels to editor

--- game/VoxelEditorCamera.gd
+++ game/VoxelEditorCamera.gd
@@ -3,7 +3,7 @@ extends Camera
 # variables:
 var old_mouse_pos = Vector2()
 var old_rotation = Vector3()
-var offset = Vector3(0, 2, 2)
+var offset = Vector3(0, 0, 3)
 
 func _ready():
 	# Called when the node is added to the scene for the first time.
@@ -11,12 +11,12 @@ func _ready():
 	pass
 
 func _process(delta):
-	if Input.is_action_just_pressed("mouse_left"):
+	if Input.is_action_just_pressed("mouse_center"):
 		old_mouse_pos = get_viewport().get_mouse_position()
 		old_rotation = self.rotation
-	if Input.is_action_pressed("mouse_left"):
-		self.rotation.y = ( old_mouse_pos.x - get_viewport().get_mouse_position().x )/100 + old_rotation.y
-		self.rotation.x = ( old_mouse_pos.y - get_viewport().get_mouse_position().y )/100 + old_rotation.x
+	if Input.is_action_pressed("mouse_center"):
+		self.rotation.y = ( old_mouse_pos.x - get_viewport().get_mouse_position().x )/200 + old_rotation.y
+		self.rotation.x = ( old_mouse_pos.y - get_viewport().get_mouse_position().y )/200 + old_rotation.x
 
 		if self.rotation.x > PI/4:
 			self.rotation.x = PI/4
@@ -25,5 +25,10 @@ func _process(delta):
 
 		var rotation_basis = Basis()
 		rotation_basis.x = offset
+		rotation_basis = rotation_basis.rotated(Vector3(1, 0, 0), self.rotation.x)
 		self.translation = rotation_basis.rotated(Vector3(0, 1, 0), self.rotation.y).x
-		self.translation.y = 0-sin(self.rotation.x*2)*2
+
+	if Input.is_action_pressed("mouse_zoom_in"):
+		offset.z = offset.z + 1
+	if Input.is_action_pressed("mouse_zoom_out"):
+		offset.z = offset.z - 1
---
+++ game/Voxels.gd
@@ -0,0 +1,20 @@
+extends Spatial
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+func _ready():
+	var voxelscene = load("res://Voxel.tscn")
+	for x in range(-10, 10):
+		for y in range(-15, 15):
+			for z in range(-10, 10):
+				var voxel = voxelscene.instance()
+				voxel.set_name("voxel_x" + str(x) + "_y" + str(y) + "_z" + str(z))
+				voxel.translate(Vector3(x, y, z))
+				call_deferred("add_child", voxel)
+
+#func _process(delta):
+#	# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+#	pass
